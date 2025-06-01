1050700612a373e8b469045201d886aa42060541
Pedro <ppagbpedro@gmail.com>
2025-06-01 20:28:18 -0300

    feat: adding api to return click consequence

--- worker/index.ts
+++ worker/index.ts
@@ -1,12 +1,17 @@
 export default {
-  fetch(request) {
+  async fetch(request, env) {
     const url = new URL(request.url);
 
-    if (url.pathname.startsWith("/api/")) {
-      return Response.json({
-        name: "Cloudflare",
-      });
+    if (url.pathname === "/api/click") {
+      const getRandomConsequenceStatement = env.DB.prepare(
+        "SELECT * FROM consequences ORDER BY RANDOM() LIMIT 1;"
+      );
+
+      const response = await getRandomConsequenceStatement.run();
+
+      return Response.json({ consequence: response.results[0].text });
     }
-		return new Response(null, { status: 404 });
+
+    return new Response(null, { status: 404 });
   },
 } satisfies ExportedHandler<Env>;
