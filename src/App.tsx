5274f3abf571302b652ee962391212b6cbd8d656
Pedro <sirpedr@Pedros-MacBook-Pro.local>
2025-06-01 18:07:11 -0300

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return <main>
+    <h1>The Unknowing Button</h1>
+    <h2>An unknown consequence on every click.</h2>
+
+    <button>Click me</button>
+  </main>
+}
+
+export default App
