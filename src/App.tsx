c4fb1c0a3b4a75b701061b66163672ab96a6077c
Pedro <ppagbpedro@gmail.com>
2025-06-01 21:14:37 -0300

    feat: adding footer and basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,34 @@ function App() {
   const { consequence, getConsequence, error } = useClickConsequence();
 
   return (
-    <main>
-      <h1>The Unknowing Button</h1>
-      <h2>An unknown consequence on every click.</h2>
+    <main className="app">
+      <h1 className="app__title">The Unknowing Button</h1>
+      <h2 className="app__subtitle">An unknown consequence on every click.</h2>
 
-      <button onClick={getConsequence}>Click me</button>
-      {consequence && <p>{consequence}</p>}
+      <button className="app__button" onClick={getConsequence}>
+        Click me
+      </button>
+
+      {consequence && !error && <p className="app__message">{consequence}</p>}
+
       {error && (
-        <p>
+        <p className="app__message app__message--error">
           Error: The Consequence Engine has temporarily run out of 'Huh?'
           moments and needs a quick spiritual reboot (which, coincidentally,
           involves you clicking the button again)
         </p>
       )}
+
+      <footer className="app__footer">
+        Made with love by{" "}
+        <a
+          href="https://github.com/SirPedr"
+          target="_blank"
+          className="app__link"
+        >
+          SirPedr
+        </a>
+      </footer>
     </main>
   );
 }
